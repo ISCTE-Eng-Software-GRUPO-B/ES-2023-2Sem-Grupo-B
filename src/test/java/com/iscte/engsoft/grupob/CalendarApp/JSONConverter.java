9f7f6c6a96761c27a1b673789e6b19111bfd5dc3
lourencosoaress <lsvos@iscte-iul.pt>
2023-04-21 00:17:38 +0100

    Foi colocada um método teste: caso a conversão foi bem sucedida (caso seja igual à conversão expectada) e se dada uma string incorreta ela lança uma exceção.

--- src/test/java/com/iscte/engsoft/grupob/CalendarApp/JSONConverter.java
+++ src/test/java/com/iscte/engsoft/grupob/CalendarApp/JSONConverter.java
@@ -1,11 +1,26 @@
 package com.iscte.engsoft.grupob.CalendarApp;
 
+import com.iscte.engsoft.grupob.CalendarApp.util.JSONConverter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 class JSONConverterTest {
 
-    String content =  "Curso,\"Unidade Curricular\",Turno,Turma,\"Inscritos no turno\",\"Dia da semana\",\"Hora inicio da aula\",\"Hora fim da aula\",\"Data da aula\",\"Sala atribuida a aula\",\"Lotacao da sala\"\n" +
+    String content = "Curso,\"Unidade Curricular\",Turno,Turma,\"Inscritos no turno\",\"Dia da semana\",\"Hora inicio da aula\",\"Hora fim da aula\",\"Data da aula\",\"Sala atribuida a aula\",\"Lotacao da sala\"\n" +
+            "\"MCP,MCTRL,MES,MEA,MPP,MS\",\"MÃƒÂ©todosdePesquisaemCiÃƒÂªnciasSociais\",00553TP02,MESA1,35,Qua,18:00:00,20:00:00,07/12/2022,C3.01,54\n" +
+            "\"MCP,MCTRL,MES,MEA,MPP,MS\",\"MÃƒÂ©todosdePesquisaemCiÃƒÂªnciasSociais\",00553TP02,MESA1,35,Qua,18:00:00,20:00:00,30/11/2022,C3.01,\n" +
+            "\"MCP,MCTRL,MES,MEA,MPP,MS\",\"MÃƒÂ©todosdePesquisaemCiÃƒÂªnciasSociais\",00553TP02,MESA1,35,Qua,18:00:00,20:00:00,23/11/2022,C3.01,54\n" +
+            "\"MCP,MCTRL,MES,MEA,MPP,MS\",\"MÃƒÂ©todosdePesquisaemCiÃƒÂªnciasSociais\",00553TP02,MESA1,35,Qua,18:00:00,20:00:00,16/11/2022,C3.01,54\n" +
+            "\"MCP,MCTRL,MES,MEA,MPP,MS\",\"MÃƒÂ©todosdePesquisaemCiÃƒÂªnciasSociais\",00553TP02,MESA1,35,Qua,18:00:00,20:00:00,09/11/2022,C3.01,54\n" +
+            "\"MCP,MCTRL,MES,MEA,MPP,MS\",\"MétodosdePesquisaemCiênciasSociais\",00553TP02,MESA1,35,Qua,18:00:00,20:00:00,02/11/2022,C3.01,54\n" +
+            "\"MCP,MCTRL,MES,MEA,MPP,MS\",\"MÃƒÂ©todosdePesquisaemCiÃƒÂªnciasSociais\",00553TP02,MESA1,35,Qua,18:00:00,20:00:00,26/10/2022,C3.01,54";
+
+    String wrongContent = "Cu,\"Unidade Curricular\",Turno,Turma,\"Inscritos no turno\",\"Dia da semana\",\"Hora inicio da aula\",\"Hora fim da aula\",\"Data da aula\",\"Sala atribuida a aula\",\"Lotacao da sala\"\n" +
             "\"MCP,MCTRL,MES,MEA,MPP,MS\",\"MÃƒÂ©todosdePesquisaemCiÃƒÂªnciasSociais\",00553TP02,MESA1,35,Qua,18:00:00,20:00:00,07/12/2022,C3.01,54\n" +
             "\"MCP,MCTRL,MES,MEA,MPP,MS\",\"MÃƒÂ©todosdePesquisaemCiÃƒÂªnciasSociais\",00553TP02,MESA1,35,Qua,18:00:00,20:00:00,30/11/2022,C3.01,\n" +
             "\"MCP,MCTRL,MES,MEA,MPP,MS\",\"MÃƒÂ©todosdePesquisaemCiÃƒÂªnciasSociais\",00553TP02,MESA1,35,Qua,18:00:00,20:00:00,23/11/2022,C3.01,54\n" +
@@ -15,8 +30,21 @@ class JSONConverterTest {
             "\"MCP,MCTRL,MES,MEA,MPP,MS\",\"MÃƒÂ©todosdePesquisaemCiÃƒÂªnciasSociais\",00553TP02,MESA1,35,Qua,18:00:00,20:00:00,26/10/2022,C3.01,54";
 
     @Test
-    void inputShouldBeValidCsvFormat(){ Assertions.assertTrue(com.iscte.engsoft.grupob.CalendarApp.util.JSONConverter.isValidCSV(content)); }
-
+    void inputShouldBeValidCsvFormat() {
+        Assertions.assertTrue(JSONConverter.isValidCSV(content));
+    }
     @Test
-    void shouldConvertFromCsvToJson(){ Assertions.assertNotNull(com.iscte.engsoft.grupob.CalendarApp.util.JSONConverter.csvToJSON(content)); }
+    void outputShouldBeAnEmptyString() {
+        Assertions.assertEquals("", JSONConverter.csvToJSON(wrongContent));
+    }
+    @Test
+    void shouldConvertFromCsvToJson() {
+        try {
+            File file = ResourceUtils.getFile("classpath:test.json");
+            String fileContent = new String(Files.readAllBytes(file.toPath()));
+            Assertions.assertEquals(fileContent, JSONConverter.csvToJSON(content));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
