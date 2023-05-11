31167df6174ed651f68a69320487c28369eff3f3
lourencosoaress <lsvos@iscte-iul.pt>
2023-05-11 02:36:48 +0100

    Foi reduzida a complexidade cognitiva da classe CSVHandler.

--- src/main/java/com/iscte/engsoft/grupob/calendarapp/util/CSVHandler.java
+++ src/main/java/com/iscte/engsoft/grupob/calendarapp/util/CSVHandler.java
@@ -15,7 +15,6 @@ public class CSVHandler {
 	private static final char CRETURN = '\r';
 	private static final char LFEED = '\n';
 	private static final char COMMENT = '#';
-
 	private boolean stripMultipleNewlines;
 
 	/**
@@ -26,7 +25,7 @@ public class CSVHandler {
 	private boolean eofSeen;
 	private Reader in;
 
-	public static Reader stripBom(String string){
+	public static Reader stripBom(String string) {
 		return new StringReader(string);
 	}
 
@@ -106,117 +105,132 @@ public class CSVHandler {
 		while ((value = in.read()) != -1) {
 			char c = (char) value;
 			switch (c) {
-			case CRETURN:
-				if (sbuf.length() > 0) {
-					fields.add(sbuf.toString());
-					sbuf.delete(0, sbuf.length());
-				}
-				return discardLinefeed(in, stripMultipleNewlines);
-
-			case LFEED:
-				if (sbuf.length() > 0) {
-					fields.add(sbuf.toString());
-					sbuf.delete(0, sbuf.length());
-				}
-				if (stripMultipleNewlines)
+				case CRETURN:
+					addLeftField(sbuf, fields);
 					return discardLinefeed(in, stripMultipleNewlines);
-				else
-					return false;
 
-			case DQUOTE: {
-				// Processing double-quoted string ..
-				while ((value = in.read()) != -1) {
-					c = (char) value;
-					if (c == DQUOTE) {
-						// Saw another double-quote. Check if
-						// another char can be read.
-						in.mark(NUMMARK);
-						if ((value = in.read()) == -1) {
-							// Nope, found EOF; means End of
-							// field, End of record and End of
-							// File
-							if (sbuf.length() > 0) {
-								fields.add(sbuf.toString());
-								sbuf.delete(0, sbuf.length());
-							}
-							return true;
-						} else if ((c = (char) value) == DQUOTE) {
-							// Found a second double-quote
-							// character. Means the double-quote
-							// is included.
-							sbuf.append(DQUOTE);
-						} else if (c == CRETURN) {
-							// Found End of line. Means End of
-							// field, and End of record.
-							if (sbuf.length() > 0) {
-								fields.add(sbuf.toString());
-								sbuf.delete(0, sbuf.length());
-							}
-							// Read and discard a line-feed if we
-							// can indeed do so.
-							return discardLinefeed(in, stripMultipleNewlines);
-						} else if (c == LFEED) {
-							// Found end of line. Means End of
-							// field, and End of record.
-							if (sbuf.length() > 0) {
-								fields.add(sbuf.toString());
-								sbuf.delete(0, sbuf.length());
-							}
-							// No need to check further. At this
-							// point, we have not yet hit EOF, so
-							// we return false.
-							if (stripMultipleNewlines)
-								return discardLinefeed(in, stripMultipleNewlines);
-							else
-								return false;
-						} else {
+				case LFEED:
+					return intToBool(treatLFeed(sbuf,fields));
 
-							in.reset();
-							break;
-						}
-					} else {
-						// Not a double-quote, so no special meaning.
-						sbuf.append(c);
+				case DQUOTE: {
+					int boolValue = treatDQuote(in,sbuf,fields);
+					if(boolValue == 1){
+						return true;
+
+					} else if (boolValue == 0){
+						return false;
 					}
 				}
-				// Hit EOF, and did not see the terminating double-quote.
-				if (value == -1) {
-					// We ignore this error, and just add whatever
-					// left as the next field.
-					if (sbuf.length() > 0) {
-						fields.add(sbuf.toString());
-						sbuf.delete(0, sbuf.length());
-					}
-					return true;
-				}
-			}
 				break;
 
-			default:
-				if (c == separator) {
-					fields.add(sbuf.toString());
-					sbuf.delete(0, sbuf.length());
-				} else {
-					/*
-					 * A comment line is a line starting with '#' with optional whitespace at the
-					 * start.
-					 */
-					if (c == COMMENT && fields.isEmpty() && sbuf.toString().trim().isEmpty()) {
-						boolean eof = skipComment(in);
-						if (eof)
-							return eof;
-						else
-							sbuf.delete(0, sbuf.length());
-						/* Continue with next line if not eof. */
-					} else
-						sbuf.append(c);
-				}
+				default:
+					if (c == separator) {
+						addLeftField(sbuf, fields);
+					} else {
+
+						if (treatDefault(sbuf, fields, c))
+							return true;
+					}
 			}
 		}
+		addLeftField(sbuf, fields);
+		return true;
+	}
+
+	private void addLeftField(StringBuilder sbuf, ArrayList<String> fields) {
 		if (sbuf.length() > 0) {
 			fields.add(sbuf.toString());
 			sbuf.delete(0, sbuf.length());
 		}
-		return true;
+	}
+
+	private boolean treatDefault(StringBuilder sbuf, ArrayList<String> fields, char c) throws IOException {
+		/*
+		 * A comment line is a line starting with '#' with optional whitespace at the
+		 * start.
+		 */
+		if (c == COMMENT && fields.isEmpty() && sbuf.toString().trim().isEmpty()) {
+			boolean eof = skipComment(in);
+			if (eof)
+				return eof;
+			else
+				sbuf.delete(0, sbuf.length());
+			/* Continue with next line if not eof. */
+		} else
+			sbuf.append(c);
+
+		return false;
+	}
+
+	private int treatDQuote(Reader in, StringBuilder sbuf, ArrayList<String> fields) throws IOException {
+		int value;
+		while ((value = in.read()) != -1) {
+			char c = (char) value;
+			if (c == DQUOTE) {
+				// Saw another double-quote. Check if
+				// another char can be read.
+				in.mark(NUMMARK);
+				if ((value = in.read()) == -1) {
+					// Nope, found EOF; means End of
+					// field, End of record and End of
+					// File
+					addLeftField(sbuf, fields);
+					return 1;
+
+				} else if ((c = (char) value) == DQUOTE) {
+					// Found a second double-quote
+					// character. Means the double-quote
+					// is included.
+					sbuf.append(DQUOTE);
+
+				} else if (c == CRETURN) {
+					// Found End of line. Means End of
+					// field, and End of record.
+					addLeftField(sbuf, fields);
+					// Read and discard a line-feed if we
+					// can indeed do so.
+					return boolToInt(discardLinefeed(in, stripMultipleNewlines));
+
+				} else if (c == LFEED) {
+					return treatLFeed(sbuf,fields);
+
+				} else {
+
+					in.reset();
+					break;
+				}
+			} else {
+				// Not a double-quote, so no special meaning.
+				sbuf.append(c);
+			}
+		}
+		// Hit EOF, and did not see the terminating double-quote.
+		if (value == -1) {
+			// We ignore this error, and just add whatever
+			// left as the next field.
+			addLeftField(sbuf, fields);
+			return 1;
+		}
+		return -1;
+	}
+
+	public int treatLFeed(StringBuilder sbuf, List<String> fields) throws IOException {
+		addLeftField(sbuf, (ArrayList<String>) fields);
+		// No need to check further. At this
+		// point, we have not yet hit EOF, so
+		// we return false.
+		if (stripMultipleNewlines)
+			return boolToInt(discardLinefeed(in, stripMultipleNewlines));
+		return 0;
+	}
+
+	public int boolToInt(boolean b){
+		// 1 - true; 0 - false
+		if(b) return 1;
+		return 0;
+	}
+
+	public boolean intToBool(int i){
+		return i == 1;
 	}
 }
